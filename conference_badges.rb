eb794faa8a34b3ff07040101a303982f512e1d6d
michael muliro <michael.muliro@student.moringaschool.com>
2022-08-28 02:37:07 +0300

    commit tree ca5f55e8ae9c7bca075b379cfc304bc760d17c35

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+
+
+
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(members)
+    members.map do |member|
+      "Hello, my name is #{member}."
+    end
+  end
+  
+  def assign_rooms(members)
+    members.map.with_index(1) do |member, room_number|
+      "Hello, #{member}! You'll be assigned to room #{room_number}!"
+    end
+  end
+  
+  def printer(members)
+    batch_badge_creator(members).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(members).each do |assignment|
+      puts assignment
+    end
+  end
